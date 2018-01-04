93f36dc3770edf95d44d61cb78caf8bfd1d284f5
winnyclaudia <32456592+winnyclaudia@users.noreply.github.com>
2018-01-04 15:18:14 +0700

    Initial commit

---
+++ js/lagu.js
@@ -0,0 +1,13 @@
+counter = { 'pelangi':0,
+ 'balon': 0,
+ 'kereta': 0,
+ 'cicak': 0,
+ }
+function tampilLagu( id ){
+ $('.bloklagu').hide();
+ $('#'+id).show();
+ counter[id]++;
+ $('#c'+id).html(counter[id]);
+ document.getElementById('info').style.display = "none";
+ document.getElementById('copyright').style.margin = "0px";
+ }
